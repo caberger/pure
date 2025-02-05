5587e2cc37d962c120796aa528f2630e94223a63
caberger <c.aberger@htl-leonding.ac.at>
2025-02-05 14:02:20 +0100

    animate user component

--- code/src/components/user/user-component.ts
+++ code/src/components/user/user-component.ts
@@ -1,19 +1,9 @@
 import { html, render } from "lib/pure-html"
+import template from "./user-component-template.html"
 
 class UserElement extends HTMLElement {
     connectedCallback() {
-        const template = html`
-            <style>
-                div {
-                    display: flex;
-                    justify-content: center;
-                }
-            </style>
-            <div>
-                <h3>TODO: implement a user table</h3>
-            </div>
-        `
-        render(template, this)
+        render(template(""), this)
     }
 }
 
